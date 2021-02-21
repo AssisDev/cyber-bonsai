1ae78baefa9185dff2e390be09126c066411d523
Marcelo Augusto Pereira de Assis <71731452+marceloapd@users.noreply.github.com>
2021-02-21 18:29:15 -0300

    sensor preciso

--- hardware/PlantaLOT/lib/UMIDADE_SOLO/umidade_solo.cpp
+++ hardware/PlantaLOT/lib/UMIDADE_SOLO/umidade_solo.cpp
@@ -8,10 +8,14 @@ float FazLeituraUmidade()
     ValorADC = analogRead(0); //1024 -> 3,3V
     UmidadePercentual = 100 * ((1024 - (float)ValorADC) / 674);
 
-    if (ValorADC <= 230)
-    {
-        UmidadePercentual = 100.0;
-    }
+  UmidadePercentual = 100 * ((980 - (float)ValorADC) / 710);
+
+  if (ValorADC <= 270)
+  {
+    UmidadePercentual = 100.0;
+  }else if (ValorADC >=1000){
+    UmidadePercentual = 0;
+  }
 
     return UmidadePercentual;
 }
